6d059519135129786aadecbc5dbab7246c6e2e7c
Alan Yu <alanyu08@gmail.com>
2025-02-02 14:33:16 -0800

    Added suroihax stuff

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow, session } = require('electron');
+const path = require('node:path');
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://suroi.io');
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -24,6 +24,13 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
+
+  const filter = {
+    urls: ['https://suroi.io/scripts/main*']
+  }
+  session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+    callback({ cancel: true });
+  });
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
--- preload.js
+++ preload.js
@@ -1,18 +1,6 @@
-/**
- * The preload script runs before `index.html` is loaded
- * in the renderer. It has access to web APIs as well as
- * Electron's renderer process modules and some polyfilled
- * Node.js functions.
- *
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
- */
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+var _int = setInterval(() => {
+  var script = document.createElement("script");
+  script.src = "https://suroihax.glitch.me/script.user.js";
+  document.head.prepend(script);
+  clearInterval(_int);
+}, 100);
